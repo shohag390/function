49239b53937102c982953922235e37707cc6230e
shohag390 <shohag390@users.noreply.github.com>
2025-02-14 16:02:54 +0600

    Leap year

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-function inchToFeet(inch) {
-    const feet = inch / 12;
-    const fullNumber = parseInt(feet);
-    const inchConvart = inch % 12;
-    const result = fullNumber + "ft " + inchConvart + "inch";
-    return result;
+// function inchToFeet(inch) {
+//     const feet = inch / 12;
+//     const fullNumber = parseInt(feet);
+//     const inchConvart = inch % 12;
+//     const result = fullNumber + "ft " + inchConvart + "inch";
+//     return result;
+// }
+
+// const shohagHeight = inchToFeet(120);
+
+// console.log(shohagHeight);
+
+
+function isLeapYear(year) {
+    if ((year % 100 !== 0 && year % 4 === 0) || (year % 400 === 0 && year % 100 === 0)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-const shohagHeight = inchToFeet(120);
+const isLeap = isLeapYear(2100)
 
-console.log(shohagHeight);
+console.log(isLeap);
